51c4e19ea3b89e283828aaafa7871d41538b6b59
Mario Solis Martinez <mario.solis.martinez@optimissa.net>
2017-06-07 17:48:43 +0200

    PHP info file

---
+++ info.php
@@ -0,0 +1,18 @@
+﻿<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>phpinfo</title>
+	</head>
+	<body>
+		<?php
+		
+		// Muestra toda la información, por defecto INFO_ALL
+		phpinfo();
+		
+		// Muestra solamente la información de los módulos.
+		// phpinfo(8) hace exactamente lo mismo.
+		phpinfo(INFO_MODULES);
+		
+		?>
+	</body>
+</html>
